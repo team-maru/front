09917aece01a430c46c7e850845c26896dc79bc6
Kim-songhui <rlathdgmlkk@naver.com>
2025-11-05 00:49:07 +0900

    feat: Poppins 폰트 적용 및 CustomText 컴포넌트 도입
    - 프로젝트 전체 폰트를 Poppins로 표준화
    - CustomText 컴포넌트를 통한 일관된 폰트 관리 시스템 구축

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,10 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded] = useFonts({
     Pretendard: require("../assets/fonts/PretendardVariable.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
   });
 
   useEffect(() => {

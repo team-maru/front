f794c7197b92e1f232d1ee5ec6561a21fdb44086
Kim-songhui <rlathdgmlkk@naver.com>
2025-11-12 22:37:41 +0900

    feat: university list 상수 추가 및 드랍다운 구현
    - 한양대 최상단 위치, 나머지 목록은 알파벳 오름차순

---
+++ constants/universities.ts
@@ -0,0 +1,582 @@
+export const UNIVERSITIES = [
+  {
+    label: "Gangneung Wonju National University",
+    value: "Gangneung Wonju National University",
+  },
+  {
+    label: "Kangwon National University",
+    value: "Kangwon National University",
+  },
+  {
+    label: "Kyungpook National University",
+    value: "Kyungpook National University",
+  },
+  {
+    label: "Gyeongsang National University",
+    value: "Gyeongsang National University",
+  },
+  {
+    label: "Kongju National University",
+    value: "Kongju National University",
+  },
+  {
+    label: "Kunsan National University",
+    value: "Kunsan National University",
+  },
+  {
+    label: "Kumoh National Institute Of Technology",
+    value: "Kumoh National Institute Of Technology",
+  },
+  {
+    label: "Mokpo National University",
+    value: "Mokpo National University",
+  },
+  {
+    label: "Mokpo National Maritime University",
+    value: "Mokpo National Maritime University",
+  },
+  {
+    label: "Pukyong National University",
+    value: "Pukyong National University",
+  },
+  {
+    label: "Pusan National University",
+    value: "Pusan National University",
+  },
+  {
+    label: "Seoul National University",
+    value: "Seoul National University",
+  },
+  {
+    label: "Sunchon National University",
+    value: "Sunchon National University",
+  },
+  {
+    label: "Andong National University",
+    value: "Andong National University",
+  },
+  {
+    label: "Chonnam National University",
+    value: "Chonnam National University",
+  },
+  {
+    label: "Jeonbuk National University",
+    value: "Jeonbuk National University",
+  },
+  {
+    label: "Jeju National University",
+    value: "Jeju National University",
+  },
+  {
+    label: "Changwon National University",
+    value: "Changwon National University",
+  },
+  {
+    label: "Chungnam National University",
+    value: "Chungnam National University",
+  },
+  {
+    label: "Chungbuk National University",
+    value: "Chungbuk National University",
+  },
+  {
+    label: "Korea National University Of Education",
+    value: "Korea National University Of Education",
+  },
+  {
+    label: "Korea National Sport University",
+    value: "Korea National Sport University",
+  },
+  {
+    label: "Korea Maritime & Ocean University",
+    value: "Korea Maritime & Ocean University",
+  },
+  {
+    label: "Korea National University Of Transportation",
+    value: "Korea National University Of Transportation",
+  },
+  {
+    label: "Gyeongnam National University Of Science And Technology",
+    value: "Gyeongnam National University Of Science And Technology",
+  },
+  {
+    label: "Seoul National University Of Science And Technology",
+    value: "Seoul National University Of Science And Technology",
+  },
+  {
+    label: "Hankyong National University",
+    value: "Hankyong National University",
+  },
+  {
+    label: "Hanbat National University",
+    value: "Hanbat National University",
+  },
+  {
+    label: "University Of Seoul",
+    value: "University Of Seoul",
+  },
+  {
+    label: "The Catholic University Of Korea",
+    value: "The Catholic University Of Korea",
+  },
+  {
+    label: "Methodist Theological University",
+    value: "Methodist Theological University",
+  },
+  {
+    label: "Kangnam University",
+    value: "Kangnam University",
+  },
+  {
+    label: "Konkuk University",
+    value: "Konkuk University",
+  },
+  {
+    label: "Konyang University",
+    value: "Konyang University",
+  },
+  {
+    label: "Kyonggi University",
+    value: "Kyonggi University",
+  },
+  {
+    label: "Kyungnam University",
+    value: "Kyungnam University",
+  },
+  {
+    label: "Kyungdong University",
+    value: "Kyungdong University",
+  },
+  {
+    label: "Daegu Haany University",
+    value: "Daegu Haany University",
+  },
+  {
+    label: "Kyungsung University",
+    value: "Kyungsung University",
+  },
+  {
+    label: "Gachon University",
+    value: "Gachon University",
+  },
+  {
+    label: "Kyungil University",
+    value: "Kyungil University",
+  },
+  {
+    label: "Kyung Hee University",
+    value: "Kyung Hee University",
+  },
+  {
+    label: "Keimyung University",
+    value: "Keimyung University",
+  },
+  {
+    label: "Korea University",
+    value: "Korea University",
+  },
+  {
+    label: "Korea University Sejong Campus",
+    value: "Korea University Sejong Campus",
+  },
+  {
+    label: "Kosin University",
+    value: "Kosin University",
+  },
+  {
+    label: "Catholic Kwandong University",
+    value: "Catholic Kwandong University",
+  },
+  {
+    label: "Kwangwoon University",
+    value: "Kwangwoon University",
+  },
+  {
+    label: "Kookmin University",
+    value: "Kookmin University",
+  },
+  {
+    label: "Dankook University",
+    value: "Dankook University",
+  },
+  {
+    label: "Daegu University",
+    value: "Daegu University",
+  },
+  {
+    label: "Daegu Arts University",
+    value: "Daegu Arts University",
+  },
+  {
+    label: "Daegu Catholic University",
+    value: "Daegu Catholic University",
+  },
+  {
+    label: "Sehan University",
+    value: "Sehan University",
+  },
+  {
+    label: "Daejeon University",
+    value: "Daejeon University",
+  },
+  {
+    label: "Daejin University",
+    value: "Daejin University",
+  },
+  {
+    label: "Seoul Christian University",
+    value: "Seoul Christian University",
+  },
+  {
+    label: "Dongguk University",
+    value: "Dongguk University",
+  },
+  {
+    label: "Dongduk Women's University",
+    value: "Dongduk Women's University",
+  },
+  {
+    label: "Dongseo University",
+    value: "Dongseo University",
+  },
+  {
+    label: "Dong-A University",
+    value: "Dong-A University",
+  },
+  {
+    label: "Myongji University",
+    value: "Myongji University",
+  },
+  {
+    label: "Mokwon University",
+    value: "Mokwon University",
+  },
+  {
+    label: "Pai Chai University",
+    value: "Pai Chai University",
+  },
+  {
+    label: "Catholic University Of Pusan",
+    value: "Catholic University Of Pusan",
+  },
+  {
+    label: "Busan University Of Foreign Studies",
+    value: "Busan University Of Foreign Studies",
+  },
+  {
+    label: "Sahmyook University",
+    value: "Sahmyook University",
+  },
+  {
+    label: "Sangmyung University",
+    value: "Sangmyung University",
+  },
+  {
+    label: "Sogang University",
+    value: "Sogang University",
+  },
+  {
+    label: "Seokyeong University",
+    value: "Seokyeong University",
+  },
+  {
+    label: "Seoul Women's University",
+    value: "Seoul Women's University",
+  },
+  {
+    label: "Sun Moon University",
+    value: "Sun Moon University",
+  },
+  {
+    label: "Sungkyul University",
+    value: "Sungkyul University",
+  },
+  {
+    label: "Sungkonghoe University",
+    value: "Sungkonghoe University",
+  },
+  {
+    label: "Sungkyunkwan University",
+    value: "Sungkyunkwan University",
+  },
+  {
+    label: "Sungshin Women'S University",
+    value: "Sungshin Women'S University",
+  },
+  {
+    label: "Sejong University",
+    value: "Sejong University",
+  },
+  {
+    label: "The University Of Suwon",
+    value: "The University Of Suwon",
+  },
+  {
+    label: "Sookmyung Women'S University",
+    value: "Sookmyung Women'S University",
+  },
+  {
+    label: "Soonchunhyang University",
+    value: "Soonchunhyang University",
+  },
+  {
+    label: "Soongsil University",
+    value: "Soongsil University",
+  },
+  {
+    label: "Silla University",
+    value: "Silla University",
+  },
+  {
+    label: "Ajou University",
+    value: "Ajou University",
+  },
+  {
+    label: "Yonsei University",
+    value: "Yonsei University",
+  },
+  {
+    label: "Yeungnam University",
+    value: "Yeungnam University",
+  },
+  {
+    label: "University Of Ulsan",
+    value: "University Of Ulsan",
+  },
+  {
+    label: "Wonkwang University",
+    value: "Wonkwang University",
+  },
+  {
+    label: "Eulji University",
+    value: "Eulji University",
+  },
+  {
+    label: "Ewha Womans University",
+    value: "Ewha Womans University",
+  },
+  {
+    label: "Inje University",
+    value: "Inje University",
+  },
+  {
+    label: "Inha University",
+    value: "Inha University",
+  },
+  {
+    label: "Jeonju University",
+    value: "Jeonju University",
+  },
+  {
+    label: "Chosun University",
+    value: "Chosun University",
+  },
+  {
+    label: "Chung-Ang University",
+    value: "Chung-Ang University",
+  },
+  {
+    label: "Baekseok University",
+    value: "Baekseok University",
+  },
+  {
+    label: "Cheongju University",
+    value: "Cheongju University",
+  },
+  {
+    label: "Chongshin University",
+    value: "Chongshin University",
+  },
+  {
+    label: "CHA University",
+    value: "CHA University",
+  },
+  {
+    label: "Pohang University Of Science And Technology (Postech)",
+    value: "Pohang University Of Science And Technology (Postech)",
+  },
+  {
+    label: "Korea University of Technology and Education",
+    value: "Korea University of Technology and Education",
+  },
+  {
+    label: "Hankuk University Of Foreign Studies",
+    value: "Hankuk University Of Foreign Studies",
+  },
+  {
+    label: "Korea Aerospace University",
+    value: "Korea Aerospace University",
+  },
+  {
+    label: "Hannam University",
+    value: "Hannam University",
+  },
+  {
+    label: "Handong Global University",
+    value: "Handong Global University",
+  },
+  {
+    label: "Hallym University",
+    value: "Hallym University",
+  },
+  {
+    label: "Hanseo University",
+    value: "Hanseo University",
+  },
+  {
+    label: "Hansung University",
+    value: "Hansung University",
+  },
+  {
+    label: "Hanshin University",
+    value: "Hanshin University",
+  },
+  {
+    label: "Hanyang University",
+    value: "Hanyang University",
+  },
+  {
+    label: "Hanyang University Erica Campus",
+    value: "Hanyang University Erica Campus",
+  },
+  {
+    label: "Honam University",
+    value: "Honam University",
+  },
+  {
+    label: "Hoseo University",
+    value: "Hoseo University",
+  },
+  {
+    label: "Hongik University",
+    value: "Hongik University",
+  },
+  {
+    label: "Namseoul University",
+    value: "Namseoul University",
+  },
+  {
+    label: "Woosong University",
+    value: "Woosong University",
+  },
+  {
+    label: "Howon University",
+    value: "Howon University",
+  },
+  {
+    label: "Chodang University",
+    value: "Chodang University",
+  },
+  {
+    label: "Chungwoon University",
+    value: "Chungwoon University",
+  },
+  {
+    label: "Kyungwoon University",
+    value: "Kyungwoon University",
+  },
+  {
+    label: "Jungwon University",
+    value: "Jungwon University",
+  },
+  {
+    label: "Gimcheon University",
+    value: "Gimcheon University",
+  },
+  {
+    label: "Songwon University",
+    value: "Songwon University",
+  },
+  {
+    label: "Tech University of Korea",
+    value: "Tech University of Korea",
+  },
+  {
+    label: "Jejuinternationaluniversity",
+    value: "Jejuinternationaluniversity",
+  },
+  {
+    label: "Ulsan National Institute of Science and Technology",
+    value: "Ulsan National Institute of Science and Technology",
+  },
+  {
+    label: "Korea Polytechnic University",
+    value: "Korea Polytechnic University",
+  },
+  {
+    label: "Daegu Cyber University",
+    value: "Daegu Cyber University",
+  },
+  {
+    label: "Seoul Digital University",
+    value: "Seoul Digital University",
+  },
+  {
+    label: "Seoul Cyber University",
+    value: "Seoul Cyber University",
+  },
+  {
+    label: "Sejong Cyber University",
+    value: "Sejong Cyber University",
+  },
+  {
+    label: "Open Cyber University Of Korea (OCU)",
+    value: "Open Cyber University Of Korea (OCU)",
+  },
+  {
+    label: "Wonkwang Digital University",
+    value: "Wonkwang Digital University",
+  },
+  {
+    label: "The Cyber University Of Korea",
+    value: "The Cyber University Of Korea",
+  },
+  {
+    label: "Hanyang Cyber University",
+    value: "Hanyang Cyber University",
+  },
+  {
+    label: "Cyber Hankuk University Of Foreign Studies",
+    value: "Cyber Hankuk University Of Foreign Studies",
+  },
+  {
+    label: "Global Cyber University",
+    value: "Global Cyber University",
+  },
+  {
+    label: "Korea Advanced Institute of Science and Technology(KAIST)",
+    value: "Korea Advanced Institute of Science and Technology(KAIST)",
+  },
+  {
+    label: "Gwangju Institute Of Science And Technology",
+    value: "Gwangju Institute Of Science And Technology",
+  },
+  {
+    label: "DGIST",
+    value: "DGIST",
+  },
+  {
+    label: "Korea National University Of Arts",
+    value: "Korea National University Of Arts",
+  },
+  {
+    label: "Incheon National University",
+    value: "Incheon National University",
+  },
+  {
+    label: "Shinhan University",
+    value: "Shinhan University",
+  },
+  {
+    label: "Korea National University Of Cultural Heritage",
+    value: "Korea National University Of Cultural Heritage",
+  },
+  {
+    label: "Korea institute of Energy Technology",
+    value: "Korea institute of Energy Technology",
+  },
+  {
+    label: "TAEJAE UNIVERSITY",
+    value: "TAEJAE UNIVERSITY",
+  },
+];

816f5986de13fa8d4499f2a0b05f88b715ecdee0
aeri0730 <dodl8549@gmail.com>
2025-11-19 15:10:05 +0900

    setting : gathering/index.tsx 파일 생성

---
+++ app/post/gathering/index.tsx
@@ -0,0 +1,13 @@
+import { colors } from "@/constants";
+import { StyleSheet, View } from "react-native";
+
+
+interface GatheringListScreenProps {}
+
+export default function GatheringListScreen({}: GatheringListScreenProps) {
+  return (
+    <View></View>
+  );
+}
+const styles = StyleSheet.create({
+});
